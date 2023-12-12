3ef210bf6925200bdb9256312a60b0248f74565c
jignasab01 <143511939+jignasab01@users.noreply.github.com>
2023-12-12 20:22:30 +0800

    Add files via upload

---
+++ app.py
@@ -0,0 +1,27 @@
+from flask import Flask, request, jsonify, render_template
+import pandas as pd
+import joblib
+from utils import TextPreprocessor
+
+app = Flask(__name__)
+model = joblib.load(open('model.joblib','rb'))
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/analyze_sentiment',methods=['POST'])
+def analyze_sentiment():
+
+    input_text = request.form['text_input']
+    predicted_sentiment = model.predict(pd.Series([input_text]))[0]
+    if predicted_sentiment == 1:
+        output = 'positive'
+    else:
+        output = 'negative'
+
+    return render_template('index.html', sentiment=f'Predicted sentiment of "{input_text}" is {output}.')
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
