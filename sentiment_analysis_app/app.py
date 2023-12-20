1e85efce84c60da68cc2781cdb7202da2de4f966
Luke-icetana <luke@icetana.com.au>
2023-12-20 22:12:55 +0800

    Update app.py

--- sentiment_analysis_app/app.py
+++ sentiment_analysis_app/app.py
@@ -1,12 +1,18 @@
 from flask import Flask, request, jsonify, render_template
+
 import pandas as pd
+from pathlib import Path
 import joblib
 
+# Perhaps put this in a configurations file
+PROJECT_ROOT_FOLDER = Path(__file__).resolve().parent
 
-app = Flask(__name__)
-with open('sentiment_analysis_app/model.joblib', 'rb') as f:
+
+# note determine how to get the projects base path
+with open(PROJECT_ROOT_FOLDER / 'model.joblib', 'rb') as f:
     model = joblib.load(f)
 
+app = Flask(__name__)
 
 @app.route('/')
 def home():
