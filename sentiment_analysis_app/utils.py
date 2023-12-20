0ea72b9de063c4ccd910af52251a3165d2e6cf26
Luke-icetana <luke@icetana.com.au>
2023-12-20 21:39:40 +0800

    Update utils.py

--- sentiment_analysis_app/utils.py
+++ sentiment_analysis_app/utils.py
@@ -9,6 +9,8 @@ from nltk.corpus import stopwords
 from nltk.tokenize import word_tokenize
 from nltk.stem import WordNetLemmatizer
 
+# Downlaod the stop words
+nltk.download('stopwords')
 
 # Define stop words
 stop_words_keywords = set(stopwords.words('english'))
